d505b341ab5d969463e61a4b8ffb109c3bf6ab15
JulioBotelho <7052768+JulioBotelho@users.noreply.github.com>
2020-04-23 10:45:58 -0300

    Divided in 2 projects
    Separate test console app from Class library

--- BasicsTorrentTest/Properties/AssemblyInfo.cs
+++ BasicsTorrentTest/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicsTorrent")]
-[assembly: AssemblyDescription("Multi torrent engine Scrapper API")]
+[assembly: AssemblyTitle("BasicsTorrentTest")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AJLBotelho")]
-[assembly: AssemblyProduct("BasicsTorrent")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicsTorrentTest")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38910152-35d5-469f-96a0-91e4e7843692")]
+[assembly: Guid("956768be-124c-4947-b567-e9031a85d7ef")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
